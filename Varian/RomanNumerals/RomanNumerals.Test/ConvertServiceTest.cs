f29e696aebc6b1b6d3d0b3760772e22df47f2875
Ivaylo Tabakov <ivo_tabako@yahoo.com>
2017-08-24 00:28:25 +0200

    added unit test project and dictioany to store mappings

---
+++ Varian/RomanNumerals/RomanNumerals.Test/ConvertServiceTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RomanNumerals;
+
+namespace RomanNumerals.Test
+{
+    [TestClass]
+    public class ConvertServiceTest
+    {
+        [TestMethod]
+        public void OneToITest()
+        {
+            ConvertService svc = new ConvertService();
+
+            string result = svc.DoTransform(1);
+
+            Assert.AreEqual("I", result);
+        }
+
+        [TestMethod]
+        public void FiveToVTest()
+        {
+            ConvertService svc = new ConvertService();
+
+            string result = svc.DoTransform(5);
+
+            Assert.AreEqual("V", result);
+        }
+
+        [TestMethod]
+        public void TenToXTest()
+        {
+            ConvertService svc = new ConvertService();
+
+            string result = svc.DoTransform(10);
+
+            Assert.AreEqual("X", result);
+        }
+
+        [TestMethod]
+        public void FiftyToLTest()
+        {
+            ConvertService svc = new ConvertService();
+
+            string result = svc.DoTransform(50);
+
+            Assert.AreEqual("L", result);
+        }
+
+        [TestMethod]
+        public void HundredToCTest()
+        {
+            ConvertService svc = new ConvertService();
+
+            string result = svc.DoTransform(100);
+
+            Assert.AreEqual("C", result);
+        }
+
+        [TestMethod]
+        public void FiveHundredToDTest()
+        {
+            ConvertService svc = new ConvertService();
+
+            string result = svc.DoTransform(500);
+
+            Assert.AreEqual("D", result);
+        }
+
+        [TestMethod]
+        public void ThousandToMTest()
+        {
+            ConvertService svc = new ConvertService();
+
+            string result = svc.DoTransform(1000);
+
+            Assert.AreEqual("M", result);
+        }
+
+    }
+}
--- Varian/RomanNumerals/RomanNumerals/ConvertService.cs
+++ Varian/RomanNumerals/RomanNumerals/ConvertService.cs
@@ -8,5 +8,23 @@ namespace RomanNumerals
 {
     public class ConvertService
     {
+        private Dictionary<int, string> mappings = new Dictionary<int, string>();
+        
+
+        public ConvertService()
+        {
+            mappings.Add(1, "I");
+            mappings.Add(5, "V");
+            mappings.Add(10, "X");
+            mappings.Add(50, "L");
+            mappings.Add(100, "C");
+            mappings.Add(500, "D");
+            mappings.Add(1000, "M");
+        }
+
+        public string DoTransform(int number)
+        {
+            return mappings[number];
+        }
     }
 }
